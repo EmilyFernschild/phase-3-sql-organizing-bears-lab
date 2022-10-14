56877d2888f80fc94a4237a4554da8f648132bdb
Emily Fernschild <emilyfernschild@gmail.com>
2022-10-14 08:33:33 -0400

    commit tree d40ccd894d4db33ac15a37973ae8c8d6e046cf57

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'Chocolate Brown', 'Warm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'Tan', 'Fun', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'White', 'Aggressive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'M', 'Brown', 'Stern', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'F', 'Light Brown', 'Sassy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'Green', 'Grouch', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'M', 'Yellow', 'Happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, Null, NULL, 1);
